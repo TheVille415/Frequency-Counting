8a820eaf4801eb8450a741849ae2fedeb1415eb6
Jordan_Torres <jt.arontorres@gmail.com>
2021-02-19 21:10:36 -0800

    second commit, adding create arr method

--- HashTable.py
+++ HashTable.py
@@ -13,9 +13,13 @@ class HashTable:
   # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
 
   def create_arr(self, size):
-    pass
+    arr = []
+    # for each element in the list you want it to be a linked list object
+    for i in range(size):
+      new_linkedlist = LinkedList()
+      arr.append(new_linkedlist)
 
-
+    return arr
 
 
   # 2️⃣ TODO: Create your own hash function.
@@ -23,8 +27,17 @@ class HashTable:
   # Hash functions are a function that turns each of these keys into an index value that we can use to decide where in our list each key:value pair should be stored. 
 
   def hash_func(self, key):
-    pass
+    # 1. Get the first letter of the key and lower case it 
+    first_letter = key[0].lower()
 
+    # 2. Calculate the distance from letter a
+    distance_from_a = ord(first_letter) - ord('a')
+
+    # 3. Mod it to make sure it is in range
+    index = distance_from_a % self.size
+
+    # returns index 
+    return index
 
 
 
@@ -33,9 +46,36 @@ class HashTable:
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
   def insert(self, key, value):
-    pass
+    # dictionaries / hash tables store "key value pairs"
+    # finding the index where key value pair should be placed
+    key_hash = self.hash_func(key)
 
+    # linear probing 
+    # check if the bucket/slot is empty
+    if self.arr[key_hash] == None:
 
+      # this assigns the key value to the slot
+      self.arr[key_hash] = (key, value)
+      return key_hash
+      
+    # if its taken (bucket) loop
+    else:
+      # pointer is going to look and see if the bucket has something in it
+      ptr = (key_hash + 1 ) % self.size
+
+      # base condition
+      while ptr != key_hash: 
+        # if the next spot is open take that
+        if self.arr[ptr] == None:
+          self.arr[ptr] = (key, value)
+          return ptr
+
+        # incrimenting pointer by 1 and making sure it doesnt exceed the size
+        else:
+          ptr = (ptr + 1) % self.size
+        
+      print("Table is full!")
+      return -1
 
 
   # 4️⃣ TODO: Complete the print_key_values method.
@@ -50,7 +90,9 @@ class HashTable:
   # erase: 2
 
   def print_key_values(self):
-    pass
-
-
-
+    if self.arr == None:
+      print("empty")
+      return -1
+    else:
+      for i in range(self.size):
+        print(i)
